ff06da6e5bf3b27b68480a5cfdadbbf8fd5a9496
ianchan3 <ianchan3138@gmail.com>
2023-01-04 13:17:25 -0800

    Day 4 of 100 Days oF JavaScript Completed- Logic Cleanup

--- js/app.js
+++ js/app.js
@@ -3,18 +3,15 @@ const modelContent = document.querySelector(".model-content");
 const close = document.querySelector(".close");
 const btn = document.getElementById("btn");
 
-btn.addEventListener("click", openModel);
-close.addEventListener("click", closeModel);
-
-function openModel(e) {
+btn.addEventListener("click", (e) => {
   e.preventDefault();
   model.style.display = "block";
-}
+});
 
-function closeModel() {
+close.addEventListener("click", () => {
   modelContent.classList.add("slide-up")
   setTimeout(() => {
     model.style.display = "none";
     modelContent.classList.remove("slide-up")
   }, 500)
-}
+});
